6c63420fa1d2fb9d46d8c67afc9368703ae95eff
Olaya <olayabaabidane@gmail.com>
2025-03-28 15:56:05 +0100

    Creación de constructor, funciones y getters y setters en Jugador

--- src/Modelo/Jugador.java
+++ src/Modelo/Jugador.java
@@ -1,5 +1,67 @@
 package Modelo;
 
+import java.util.ArrayList;
+
 public class Jugador {
+
+	String nombre;
+	String colorFicha;
+	int posicion;
+	ArrayList<Dado> dados = new ArrayList<Dado>();
+	ArrayList<Pez> peces = new ArrayList<Pez>();
+	ArrayList<BolaNieve> bolasNieve = new ArrayList<BolaNieve>();
+
+	// CONSTRUCTOR
+	public Jugador(String nombre, String colorFicha, int posicion, ArrayList<Dado> dados, ArrayList<Pez> peces,
+			ArrayList<BolaNieve> bolasNieve) {
+		this.nombre = nombre;
+		this.colorFicha = colorFicha;
+		this.posicion = posicion;
+		this.dados = dados;
+		this.peces = peces;
+		this.bolasNieve = bolasNieve;
+	}
+
+	// FUNCIONES
+	public void tirarDado() {
+
+	}
+
+	public void usarBolaNieve(/* Poner aqui dentro "jugador" */) {
+
+	}
+
+	public void usarPez() {
+
+	}
+
+	public void avanzar() {
+
+	}
+
+	// GETTERS Y SETTERS
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getColorFicha() {
+		return colorFicha;
+	}
+
+	public void setColorFicha(String colorFicha) {
+		this.colorFicha = colorFicha;
+	}
+
+	public int getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(int posicion) {
+		this.posicion = posicion;
+	}
 
 }
