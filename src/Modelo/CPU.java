e97f327ec975bbbf355ca15b532322d515ab1cb9
Ares <aresgp5335@gmail.com>
2025-03-28 16:05:30 +0100

    Creación de constructor y fuciones

--- src/Modelo/CPU.java
+++ src/Modelo/CPU.java
@@ -1,5 +1,25 @@
 package Modelo;
 
-public class CPU {
+import java.util.ArrayList;
 
+public class CPU extends Jugador{
+
+	// CONSTRUCTOR
+	public CPU(String nombre, String colorFicha, int posicion, ArrayList<Dado> dados, ArrayList<Pez> peces, ArrayList<BolaNieve> bolasNieve) {
+		super(nombre, colorFicha, posicion, dados, peces, bolasNieve);
+		// TODO Auto-generated constructor stub
+	}
+
+	// FUNCIONES
+	public void tirarDado() {
+		
+	}
+	
+	public void atacarJugador(ArrayList<Jugador> jugadores) {
+		
+	}
+	
+	public void bloqueo() {
+		
+	}
 }
