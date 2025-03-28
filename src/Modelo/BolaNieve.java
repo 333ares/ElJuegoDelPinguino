d78bc9f0b6d08f7bc8f3e6a2eda944fdf21d03ea
Ares <aresgp5335@gmail.com>
2025-03-28 16:11:37 +0100

    Creación de constructor, funciones y getters y setters de BolaNieve

--- src/Modelo/BolaNieve.java
+++ src/Modelo/BolaNieve.java
@@ -2,4 +2,24 @@ package Modelo;
 
 public class BolaNieve {
 
+	int cantidad;
+
+	// CONSTRUCTOR
+	public BolaNieve(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	// FUNCIONES
+	public void usar() {
+
+	}
+
+	// GETTERS Y SETTERS
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
 }
