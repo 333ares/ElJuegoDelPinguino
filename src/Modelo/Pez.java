2a13310a64126cac009fa71dc5a2992965775e45
Ares <aresgp5335@gmail.com>
2025-03-28 16:11:53 +0100

    Creación de constructor, funciones y getters y setters de Pez

--- src/Modelo/Pez.java
+++ src/Modelo/Pez.java
@@ -2,4 +2,24 @@ package Modelo;
 
 public class Pez {
 
+	int cantidad;
+
+	// CONSTRUCTOR
+	public Pez(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	// FUNCIONES
+	public void usar() {
+
+	}
+
+	// GETTERS Y SETTERS
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
 }
