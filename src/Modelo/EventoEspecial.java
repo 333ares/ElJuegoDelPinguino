5fa08d76ddc1981787172f7abc7ff7037434ffbb
Olaya <olayabaabidane@gmail.com>
2025-03-28 16:13:45 +0100

    Creación de constructor, funciones y getters y setters en EventoEspecial

--- src/Modelo/EventoEspecial.java
+++ src/Modelo/EventoEspecial.java
@@ -1,5 +1,28 @@
 package Modelo;
 
+import java.util.ArrayList;
+
 public class EventoEspecial {
+
+	String descripción;
+
+	// CONSTRUCTOR
+	public EventoEspecial(String descripción) {
+		this.descripción = descripción;
+	}
+
+	// FUNCIONES
+	public void activar(ArrayList<Jugador> jugadores) {
+
+	}
+
+	// GETTERS Y SETTERS
+	public String getDescripción() {
+		return descripción;
+	}
+
+	public void setDescripción(String descripción) {
+		this.descripción = descripción;
+	}
 
 }
