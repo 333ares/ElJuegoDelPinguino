316a708be2bf7393c9a3ff6878f4a0edde1b945f
Ares <aresgp5335@gmail.com>
2025-03-28 16:14:52 +0100

    Creación de constructor, funciones y getters y setters de Tablero

--- src/Modelo/Tablero.java
+++ src/Modelo/Tablero.java
@@ -1,5 +1,31 @@
 package Modelo;
 
+import java.util.ArrayList;
+
 public class Tablero {
 
+	ArrayList<Casilla> casillas = new ArrayList<Casilla>();
+	
+	// CONSTRUCTOR
+	public Tablero(ArrayList<Casilla> casillas) {
+		this.casillas = casillas;
+	}
+	
+	// FUNCIONES
+	public void generarTablero() {
+		
+	}
+	
+	public void mostrarTablero() {
+		
+	}
+	
+	// GETTERS Y SETTERS
+	public ArrayList<Casilla> getCasillas() {
+		return casillas;
+	}
+
+	public void setCasillas(ArrayList<Casilla> casillas) {
+		this.casillas = casillas;
+	}
 }
