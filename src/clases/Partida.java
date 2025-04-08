83459917ccc28463234b90c3b2da43a18b9e36af
Ares <aresgp5335@gmail.com>
2025-04-08 16:30:17 +0200

    Cambios en la clase Partida.

--- src/clases/Partida.java
+++ src/clases/Partida.java
@@ -4,47 +4,30 @@ import java.util.ArrayList;
 
 public class Partida {
 
-	ArrayList<GestorJugador> jugadores = new ArrayList<GestorJugador>();
-	ArrayList<CPU> cpu = new ArrayList<CPU>();
-	int[][] tablero;
-	int turnoActual;
-
-	// CONSTRUCTOR
-	public Partida(ArrayList<GestorJugador> jugadores, ArrayList<CPU> cpu, int[][] tablero, int turnoActual) {
-		this.jugadores = jugadores;
-		this.cpu = cpu;
-		this.tablero = tablero;
-		this.turnoActual = turnoActual;
+	Tablero tablero;
+	
+	public void btnTirarDado() {
+		
 	}
-
-	// FUNCIONES
-	public void guardarPartida() {
-
+	
+	public void btnUsarObjeto() {
+		
 	}
-
+	
+	public void btnFinalizarTurno() {
+		
+	}
+	
+	public void iniciarPartida () {
+		
+	}
+	
 	public void cargarPartida() {
-
+		
 	}
-
-	public void siguienteTurno() {
-
-	}
-
-	// GETTERS Y SETTERS
-	public int[][] getTablero() {
-		return tablero;
-	}
-
-	public void setTablero(int[][] tablero) {
-		this.tablero = tablero;
-	}
-
-	public int getTurnoActual() {
-		return turnoActual;
-	}
-
-	public void setTurnoActual(int turnoActual) {
-		this.turnoActual = turnoActual;
+	
+	public void guardarPartida(Tablero t) {
+		
 	}
 
 }
