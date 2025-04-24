53c7c55a74f325016ae5137ee229a0b2c617c8db
Ares <aresgp5335@gmail.com>
2025-04-24 19:43:33 +0200

    Herencia añadida
    He añadido la herencia de la clase Casilla.

--- src/clases/Trineo.java
+++ src/clases/Trineo.java
@@ -1,9 +1,16 @@
 package clases;
 
-public class Trineo {
-	
-	public void realizarAccion() {
+import java.util.ArrayList;
 
+public class Trineo extends Casilla {
+	
+	public Trineo(int posicion, ArrayList<Jugador> jugadoresActuales) {
+		super(posicion, jugadoresActuales);
+
+	}
+
+	public void realizarAccion(Jugador j) {
+		
 	}
 
 }
