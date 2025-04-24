3c8d45bce762fb0450736defe6dc4c348060f5a4
Olaya <olayabaabidane@gmail.com>
2025-04-24 20:00:05 +0200

    actualizar

---
+++ src/modelo/CasillaNormal.java
@@ -0,0 +1,13 @@
+package modelo;
+
+public class CasillaNormal extends Casilla {
+
+    public CasillaNormal(int posicion) {
+        super(posicion);
+    }
+    
+    public void realizarAccion(Jugador j) {
+        
+    }
+
+}
