f219d88ab561089917e5d7a3152a8672f43fc493
Ares <aresgp5335@gmail.com>
2025-04-24 19:57:33 +0200

    Herencia añadida

--- src/modelo/CasillaNormal.java
+++ src/modelo/CasillaNormal.java
@@ -1,12 +1,9 @@
 package modelo;
 
-import java.util.ArrayList;
-
 public class CasillaNormal extends Casilla {
 
-	public CasillaNormal(int posicion, ArrayList<Jugador> jugadoresActuales) {
-		super(posicion, jugadoresActuales);
-		// TODO Auto-generated constructor stub
+	public CasillaNormal(int posicion) {
+		super(posicion);
 	}
 	
 	public void realizarAccion(Jugador j) {
