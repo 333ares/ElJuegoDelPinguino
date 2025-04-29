b902c524d203ef28b01c6b0b09e09cf14770c80a
Olaya <olayabaabidane@gmail.com>
2025-04-29 17:03:33 +0200

    Lógica de agujero

--- src/modelo/Agujero.java
+++ src/modelo/Agujero.java
@@ -8,6 +8,10 @@ public class Agujero extends Casilla {
 	}
 
 	public void realizarAccion(Jugador j) {
+		System.out.println("¡Caíste en un agujero! Retrocedes 5 casillas.");
+        int nuevaPosicion = j.getPosicion() - 5;
+        if (nuevaPosicion < 0) nuevaPosicion = 0;
+        j.setPosicion(nuevaPosicion);
 
 	}
 
