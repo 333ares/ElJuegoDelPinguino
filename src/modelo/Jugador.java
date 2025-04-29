a7e8656a51ffe378f2ffdf57a2e56bbf076d585b
Olaya <olayabaabidane@gmail.com>
2025-04-29 16:44:53 +0200

    metodo getPinguino en la clase Jugador
    El método getPinguino se utiliza para obtener el objeto Pinguino asociado al jugador.

--- src/modelo/Jugador.java
+++ src/modelo/Jugador.java
@@ -7,16 +7,24 @@ public abstract class Jugador extends GestorJugador {
 	int posicion;
 	String nombre;
 	String color;
+	Pinguino pinguino;
 
 	// CONSTRUCTOR
-	public Jugador(int posicion, String nombre, String color) {
+	public Jugador(int posicion, String nombre, String color, Pinguino pinguino) {
 		super();
 		this.posicion = posicion;
 		this.nombre = nombre;
 		this.color = color;
+		this.pinguino = pinguino;
 	}
 
 	// GETTERS Y SETTERS
+	public Pinguino getPinguino() { // El método getPinguino se utiliza para obtener el objeto Pinguino asociado al jugador.
+		return pinguino;
+	}
+	public Pinguino setPinguino() {
+		return pinguino;
+	}
 	public int getPosicion() {
 		return posicion;
 	}
