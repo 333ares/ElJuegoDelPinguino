d62f1f775835d02841622c80ba0c5c5e334a0a1e
Olaya <olayabaabidane@gmail.com>
2025-04-29 17:11:37 +0200

    clase trineo

--- src/modelo/Trineo.java
+++ src/modelo/Trineo.java
@@ -1,1 +1,21 @@
+package modelo;
 
+public class Trineo extends Casilla {
+
+	public Trineo(int posicion) {
+		super(posicion);
+	}
+
+	public void realizarAccion(Jugador j) {
+		
+		 System.out.println("¡Evento aleatorio!");
+		 System.out.println("¡Enhorabuena! Has encontrado un trineo. Avanzas 10 casillas.");
+	        int nuevaPosicion = j.getPosicion() + 10;
+	        if (nuevaPosicion >= 50) nuevaPosicion = 49;
+	        j.setPosicion(nuevaPosicion);
+
+
+	}
+	
+	
+}
