616373b61455094d32ece9c899de207ab4c79d62
Ares <aresgp5335@gmail.com>
2025-05-08 16:56:41 +0200

    imports innecesarios borrados

--- src/controlador/GestorPartidas.java
+++ src/controlador/GestorPartidas.java
@@ -1,16 +1,4 @@
 package controlador;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-import bbdd.bbdd;
-import modelo.Casilla;
-import modelo.Inventario;
-import modelo.Jugador;
-import modelo.Pinguino;
 import modelo.Tablero;
 
 public class GestorPartidas {
