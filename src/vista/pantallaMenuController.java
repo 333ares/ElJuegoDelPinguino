442ad1b5cb0e84a52302818649885c64a6cbd921
Ares <aresgp5335@gmail.com>
2025-05-19 16:57:07 +0200

    Variables y método "initializeController"

--- src/vista/pantallaMenuController.java
+++ src/vista/pantallaMenuController.java
@@ -1,5 +1,19 @@
 package vista;
 
+import java.sql.Connection;
+
+import bbdd.bbdd;
+import modelo.Jugador;
+import modelo.Tablero;
+
 public class pantallaMenuController {
+	private Connection con;
+    private Jugador jugador;
+    private Tablero tablero;
     
+    public void initializeController(Jugador jugador, Tablero tablero) {
+        this.con = bbdd.conectarBaseDatos();
+        this.jugador = jugador;
+        this.tablero = tablero;
+    }
 }
