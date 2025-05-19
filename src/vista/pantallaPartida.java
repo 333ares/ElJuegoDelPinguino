9bf513c9c34eb762b2cdecec838397ba290583f3
Ares <aresgp5335@gmail.com>
2025-05-19 16:02:36 +0200

    Tabulación y modificación del constructor en la clase gestorJugador

--- src/vista/pantallaPartida.java
+++ src/vista/pantallaPartida.java
@@ -1,32 +1,36 @@
 package vista;
 
 import java.io.IOException;
-import java.util.ArrayList;
-
 import controlador.GestorJugador;
 import controlador.GestorTablero;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import modelo.Casilla;
 import modelo.Jugador;
 import modelo.Tablero;
 
 public class pantallaPartida {
 
+	/*
+	 * Esta clase representa el punto de entrada para iniciar una nueva partida.
+	 * 
+	 * Se encarga de inicializar el modelo (Tablero y Jugador), cargar la vista
+	 * (FXML) y vincularla con el controlador correspondiente.
+	 */
 	public void iniciarPartida() {
-		  Tablero tablero = new Tablero();
-	        GestorTablero gestorTablero = new GestorTablero(tablero);
-	        Jugador jugadorActual = tablero.inicializarJugador(); 
+		Tablero tablero = new Tablero();
+		GestorTablero gestorTablero = new GestorTablero(tablero);
+		Jugador jugadorActual = tablero.inicializarJugador();
 
 		// Cargar la pantalla de juego y pasar las dependencias al controlador
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/pantallaJuego.fxml"));
 		Parent root;
 		try {
 			root = loader.load();
-            pantallaJuegoController controller = loader.getController();
-			controller.initializeController(new GestorJugador(jugadorActual, tablero), gestorTablero, jugadorActual, tablero);
+			pantallaJuegoController controller = loader.getController();
+			controller.initializeController(new GestorJugador(jugadorActual, tablero), gestorTablero, jugadorActual,
+					tablero);
 
 			Scene scene = new Scene(root);
 			Stage stage = new Stage();
